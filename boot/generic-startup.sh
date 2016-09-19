84f6be3e4718d800fe0e69bf35989cfd6c16d638
Devin Hughes <devin@jd2.com>
2016-09-18 21:38:26 -0400

    Add autolink of config directory

--- boot/generic-startup.sh
+++ boot/generic-startup.sh
@@ -1,14 +1,5 @@
 #!/bin/sh -e
 
-#eMMC flasher just exited single user mode via: [exec /sbin/init]
-#as we can't shudown properly in single user mode..
-unset are_we_flasher
-are_we_flasher=$(grep init-eMMC-flasher /proc/cmdline || true)
-if [ ! "x${are_we_flasher}" = "x" ] ; then
-	systemctl poweroff || halt
-	exit
-fi
-
 #Regenerate ssh host keys
 if [ -f /etc/ssh/ssh.regenerate ] ; then
 	echo "generic-board-startup: regenerating ssh keys"
@@ -47,30 +38,18 @@ if [ -f /resizerootfs ] ; then
 	sync
 fi
 
-if [ -f /proc/device-tree/model ] ; then
-	board=$(cat /proc/device-tree/model | sed "s/ /_/g")
-	echo "generic-board-startup: [model=${board}]"
-
-	case "${board}" in
-	TI_AM335x_Beagle*|TI_AM335x_Arduino_Tre|Arrow_BeagleBone_Black_Industrial|SanCloud_BeagleBone_Enhanced)
-		script="am335x_evm.sh"
-		;;
-	TI_AM5728_BeagleBoard-X15)
-		script="beagle_x15.sh"
-		;;
-	TI_OMAP3_Beagle*)
-		script="omap3_beagle.sh"
-		;;
-	TI_OMAP5_uEVM_board)
-		script="omap5_uevm.sh"
-		;;
-	*)
-		script="generic.sh"
-		;;
-	esac
-
 	if [ -f "/opt/scripts/boot/${script}" ] ; then
 		echo "generic-board-startup: [startup script=/opt/scripts/boot/${script}]"
 		/bin/sh /opt/scripts/boot/${script}
+	fi
+fi
+
+# Copy the config directory to tmpfs
+if [ -d "/home/mdadmn/machinekit/configs/jd2" ]; then
+	echo "generic-board-startup: setting up config directory"
+	cp -r "/home/mdadmn/machinekit/configs/jd2" /tmp/
+	# Create the symlink if it doesn't exist already
+	if [ ! -L "/home/mdadmn/jd2" ] ; then
+		ln -s "/tmp/jd2" "/home/mdadmn/jd2"
 	fi
 fi
